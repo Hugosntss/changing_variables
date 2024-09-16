3bbf7bea23bc0019b3b7fb24fc2037e61f1b86f6
Hugo <hugos@Hugos-MacBook-Air.local>
2024-09-15 18:56:36 -0700

    changed data type

---
+++ debuging_code.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+
+/*
+Previous variables:
+    int x=10;
+
+    int y=3;
+
+listed as ints, which can return a int value even 
+if it gets used in a new doube variable. 
+
+So we have to change the first variables
+data types of x and y to doube data types.
+
+*/
+    double x=10;  // int switched to double
+
+    double y=3;
+
+    double z= x/y;
+
+    cout <<z<<endl;
+
+return 0;
+
+}   
+
